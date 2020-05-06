41f6d483aaa4b225f6580a32237e7dfac1dba4e2
Clebert Figueiredo <cleberts.figueiredo@outlook.com>
2020-05-06 15:39:45 -0300

    Go 🚀

---
+++ precache-manifest.0630dc0e185dfb294dcc15ac8ed98445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea1e639d06739f72b7aad531d05efb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06a8657fca630501b07e",
+    "url": "/static/js/2.effbf674.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.effbf674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9d93b0f272ce804248",
+    "url": "/static/js/main.8d954a5c.chunk.js"
+  },
+  {
+    "revision": "9b670e8740f34007db08",
+    "url": "/static/js/runtime-main.0dc97fc6.js"
+  },
+  {
+    "revision": "992b5b1dd8624e5cd3b4aebe23e5e420",
+    "url": "/static/media/bg-dev-min.992b5b1d.jpg"
+  },
+  {
+    "revision": "a69c8c85379a9ef6730abef1a705e4e1",
+    "url": "/static/media/clebert-figueiredo.a69c8c85.jpg"
+  }
+]);
